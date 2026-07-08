9e1d8c9816f1a311657d99b95bf6086a870acd1c
tahmids55 <tahmid.tts@gmail.com>
2026-07-08 21:49:19 +0600

    fix: remove corrupt and failed PostgreSQL database backup files

--- storage/app/backups/markscraft_backup_2026-07-05_21-34-17.sql
+++
@@ -1,2 +0,0 @@
-pg_dump: error: server version: 17.6; pg_dump version: 14.23 (Ubuntu 14.23-0ubuntu0.22.04.1)
-pg_dump: error: aborting because of server version mismatch
--- storage/app/backups/test.sql
+++
@@ -1,2 +0,0 @@
-pg_dump: error: aborting because of server version mismatch
-pg_dump: detail: server version: 17.6; pg_dump version: 16.14 (Ubuntu 16.14-0ubuntu0.24.04.1)
