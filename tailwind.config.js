a05d2759250f60ad9a7e7d26ebcfd2e19c423b52
tahmids55 <tahmid.tts@gmail.com>
2026-08-08 12:11:22 +0600

    feat: include PHP files in Tailwind content paths for class scanning

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ export default {
         './resources/**/*.blade.php',
         './resources/**/*.js',
         './resources/**/*.vue',
+        './app/**/*.php',
     ],
     darkMode: 'class',
     theme: {
